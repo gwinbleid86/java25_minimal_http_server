1c7c61e0778c1181c7370b4fba94e0afc85ff852
Aleksei Podstavin <gwinbleid86@gmail.com>
2025-06-24 20:53:39 +0600

    lesson 45

---
+++ src/kg/attractor/java/Main.java
@@ -0,0 +1,15 @@
+package kg.attractor.java;
+
+import kg.attractor.java.lesson44.Lesson45Server;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            new Lesson45Server("localhost", 8089).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/kg/attractor/java/lesson44/Lesson45Server.java
@@ -0,0 +1,43 @@
+package kg.attractor.java.lesson44;
+
+import com.sun.net.httpserver.HttpExchange;
+import kg.attractor.java.server.ContentType;
+import kg.attractor.java.server.ResponseCodes;
+import kg.attractor.java.utils.Utils;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Map;
+
+public class Lesson45Server extends Lesson44Server{
+    public Lesson45Server(String host, int port) throws IOException {
+        super(host, port);
+        registerGet("/login", this::loginGet);
+        registerPost("/login", this::loginPost);
+    }
+
+    private void loginPost(HttpExchange exchange) {
+//        String cType = getContentType(exchange);
+//        String raw = getBody(exchange);
+//
+//        Map<String, String> parsed = Utils.parseUrlEncoded(raw, "&");
+//
+//        String fmt = "<p>Необработанные данные: <b>%s</b></p>" +
+//                "<p>Content-Type: <b>%s</b></p>" +
+//                "<p>После обработки: <b>%s</b></p>";
+//        String data = String.format(fmt, raw, cType, parsed);
+//
+//        try{
+//            sendByteData(exchange, ResponseCodes.OK, ContentType.TEXT_HTML, data.getBytes());
+//        } catch (IOException e){
+//            e.printStackTrace();
+//        }
+
+        redirect303(exchange, "/");
+    }
+
+    private void loginGet(HttpExchange exchange) {
+        Path path = makeFilePath("login.ftlh");
+        sendFile(exchange, path, ContentType.TEXT_HTML);
+    }
+}
